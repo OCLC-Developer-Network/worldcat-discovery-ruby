4cef1bf0e1537ea3774c537c1215ed953c18d963
meyerst <meyerst@oclc.org>
2014-05-06 23:09:58 -0400

    Formatting update for URIs

--- lib/worldcat/discovery/uris.rb
+++ lib/worldcat/discovery/uris.rb
@@ -1,21 +1,21 @@
 module WorldCat
   module Discovery
     
-    GENERIC_RESOURCE  = RDF::URI.new('http://www.w3.org/2006/gen/ont#ContentTypeGenericResource')
-    SCHEMA_ABOUT      = RDF::URI.new('http://schema.org/about')
-    SCHEMA_AUTHOR     = RDF::URI.new('http://schema.org/author')
-    SCHEMA_NAME       = RDF::URI.new('http://schema.org/name')
-    SCHEMA_BOOK       = RDF::URI.new('http://schema.org/Book')
-    SCHEMA_PERSON     = RDF::URI.new('http://schema.org/Person')
-    SCHEMA_INTANGIBLE = RDF::URI.new('http://schema.org/Intangible')
-    WORK_EXAMPLE      = RDF::URI.new('http://schema.org/workExample')
-    EXAMPLE_OF_WORK   = RDF::URI.new('http://schema.org/exampleOfWork')
-    NUMBER_OF_PAGES   = RDF::URI.new('http://schema.org/numberOfPages')
-    DATE_PUBLISHED    = RDF::URI.new('http://schema.org/datePublished')
-    IN_LANGUAGE       = RDF::URI.new('http://schema.org/inLanguage')
-    SCHEMA_PUBLISHER  = RDF::URI.new('http://schema.org/publisher')
-    OWL_SAME_AS       = RDF::URI.new('http://www.w3.org/2002/07/owl#sameAs')
-    OCLC_NUMBER       = RDF::URI.new('http://purl.org/library/oclcnum')
+    GENERIC_RESOURCE       = RDF::URI.new('http://www.w3.org/2006/gen/ont#ContentTypeGenericResource')
+    SCHEMA_ABOUT           = RDF::URI.new('http://schema.org/about')
+    SCHEMA_AUTHOR          = RDF::URI.new('http://schema.org/author')
+    SCHEMA_NAME            = RDF::URI.new('http://schema.org/name')
+    SCHEMA_BOOK            = RDF::URI.new('http://schema.org/Book')
+    SCHEMA_PERSON          = RDF::URI.new('http://schema.org/Person')
+    SCHEMA_INTANGIBLE      = RDF::URI.new('http://schema.org/Intangible')
+    SCHEMA_WORK_EXAMPLE    = RDF::URI.new('http://schema.org/workExample')
+    SCHEMA_EXAMPLE_OF_WORK = RDF::URI.new('http://schema.org/exampleOfWork')
+    SCHEMA_NUMBER_OF_PAGES = RDF::URI.new('http://schema.org/numberOfPages')
+    SCHEMA_DATE_PUBLISHED  = RDF::URI.new('http://schema.org/datePublished')
+    SCHEMA_IN_LANGUAGE     = RDF::URI.new('http://schema.org/inLanguage')
+    SCHEMA_PUBLISHER       = RDF::URI.new('http://schema.org/publisher')
+    OWL_SAME_AS            = RDF::URI.new('http://www.w3.org/2002/07/owl#sameAs')
+    OCLC_NUMBER            = RDF::URI.new('http://purl.org/library/oclcnum')
     
   end
 end
